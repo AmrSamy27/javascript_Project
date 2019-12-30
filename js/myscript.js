109bab9251d0aa9a8c0be21cacde20e864264496
mohamed <mohammedgammal787@gmail.com>
2019-12-31 00:38:07 +0200

    mohamed files of javascript project

---
+++ js/myscript.js
@@ -0,0 +1,46 @@
+
+
+let submi=document.getElementById("formButton");
+submi.addEventListener('click',viewdata);
+
+
+function viewdata(e)
+{
+    e.preventDefault();
+    let name=document.getElementById("formName").value;
+    let email=document.getElementById("formEmail").value;
+    let subject=document.getElementById("formSubject").value;
+    let message=document.getElementById("formMessage").value;  
+
+    var objectOfIformation = {
+      name: name,
+       email: email,
+       subject: subject,
+      message: message
+     };
+        
+
+     fetch('http://afternoon-falls-30227.herokuapp.com/api/v1/contact_us',{ 
+
+
+method : 'POST',
+headers: {
+  'Content-Type': 'application/json'
+  // 'Content-Type': 'application/x-www-form-urlencoded',
+},
+body : JSON.stringify(objectOfIformation),
+
+
+
+
+     }).then(function (resolve) {
+       //console.log(JSON.resolve);
+       console.log(resolve)
+      }).catch(function (reject){console.log( reject)});
+
+
+
+}
+
+
+
